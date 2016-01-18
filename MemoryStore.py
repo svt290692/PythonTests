1abba9c02c1e7bf98d43c68545c93187f3780cf6
vmalysh <vmalysh@mera.com>
2016-01-18 15:30:25 +0300

    continue working

--- MemoryStore.py
+++ MemoryStore.py
@@ -1,5 +1,39 @@
 from Tkinter import *
 
+elements = {}
+
+
+class ModifiedMixin:
+    def _init(self):
+        self.clearModifiedFlag()
+        self.bind_all('<<Modified>>', self._beenModified)
+
+    def _beenModified(self, event=None):
+        if self._resetting_modified_flag: return
+        self.clearModifiedFlag()
+        self.beenModified(event)
+
+    def beenModified(self, event=None):
+        pass
+
+    def clearModifiedFlag(self):
+        self._resetting_modified_flag = True
+        try:
+            self.tk.call(self._w, 'edit', 'modified', 0)
+        finally:
+            self._resetting_modified_flag = False
+
+
+class TextWithModifyListener(ModifiedMixin, Text):
+    def __init__(self, *a, **b):
+        Text.__init__(self, *a, **b)
+        self._init()
+
+    def setTextChangedListener(self, listener):
+        self.listener = listener
+
+    def beenModified(self, event=None):
+        self.listener()
 
 class TopSideSection:
     def __init__(self, master):
@@ -39,21 +73,35 @@ class TopSideSection:
         self.addButton.grid(row=3, column=4,sticky=W)
 
 def addButtonPushed():
-    pass
+    title = topSection.titleSearchEntry.get()
+    tags = topSection.tagSearchEntry.get()
+
+    elementList.insert(ACTIVE, "title")
+    elements[title] = ""
 
 def searchButtonPushed():
     pass
+
+
+def synchronizeElements():
+    pass
+
+
+def textAreaTextChanged():
+    print("Yes")
 
 root = Tk()
 
 topSection = TopSideSection(root)
 
-elementList = Listbox(root, bg="#8b0000")
+elementList = Listbox(root, bg="#8b0000", fg="white")
 elementList.pack(side=LEFT,expand=True,fill=BOTH)
 
-textArea = Text(root, bg="#ffc966")
+textArea = TextWithModifyListener(root, bg="#ffc966")
+textArea.setTextChangedListener(textAreaTextChanged)
 textArea.pack(side=LEFT,expand=True, fill=BOTH)
 
-topSection.addButton.bind("<Button-1>",addButtonPushed)
+topSection.addButton.config(command=addButtonPushed)
+topSection.searchButton.config(command=searchButtonPushed)
 
 mainloop()
